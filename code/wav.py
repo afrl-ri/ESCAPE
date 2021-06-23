cb8ec4302f6d9f1320d7da857e42cb6dc1d44ad6
planchock <78160358+planchock@users.noreply.github.com>
2021-06-23 05:36:45 -0400

    Create wav.py

---
+++ code/wav.py
@@ -0,0 +1,25 @@
+# file path for .wav data 
+wavZer = 'd31s05r0520181018_0.wav'
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import wave as wv
+from scipy.io.wavfile import read
+# Read in Data using the wave library
+datZer = wv.open(wavZer,'r')
+# get and print wav info
+datZerParam = datZer.getparams()
+# initialize list of usefule information 
+print("Data Set 1: ", datZerParam, "\n")
+# read audio samples
+graphZer = read(wavZer)
+audio = graphZer[1]
+# plot the first 1024 samples
+plt.plot(audio[0:datZer.getnframes()])
+# label the axes
+plt.ylabel("Amplitude")
+plt.xlabel("Time")
+# set the title  
+plt.title("Sample Wave, device %s%s" %(wavZer[1], wavZer[2]))
+# display the plot
+plt.show()
